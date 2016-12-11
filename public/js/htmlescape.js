c3ce381ec1a229676991021f297228fd341f41e4
David Meyer <pdox@alum.mit.edu>
2016-12-11 13:00:25 -0800

    Add HTML escaping on untrusted server-provided input

---
+++ public/js/htmlescape.js
@@ -0,0 +1,8 @@
+function htmlEscape(s) {
+  return String(s)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
